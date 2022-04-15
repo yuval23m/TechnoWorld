4453f44c0d16ee51245424d08b2f2a9fd1ed9d63
lete <103456882+LETEX007@users.noreply.github.com>
2022-04-15 14:22:25 -0400

    Validacion clave funcionando

--- js/app.js
+++ js/app.js
@@ -4,13 +4,17 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // LETRAS Y ESPACIOS, PUEDEN LLEVAR ACENTOS
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{8,9}$/ // 8 A 9 NUMEROS
+    telefono: /^\d{8,9}$/, // 8 A 9 NUMEROS
+    clave: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    clave2: /^[a-zA-ZÀ-ÿ\s]{1,40}$/
 }
 
 const campos = { //CAMPOS EN FALSO QUE AL MOMENTO DE VALIDAR CAMBIARAN
     nombre: false,
     correo: false,
-    telefono: false
+    telefono: false,
+    clave: false,
+    clave2: false
 }
 
 const validarFormulario = (e) => {
@@ -24,6 +28,30 @@ const validarFormulario = (e) => {
         case "telefono":
             validarCampo(expresiones.telefono, e.target, 'telefono');
             break;
+        case "clave":
+            validarCampo(expresiones.telefono, e.target, 'clave');
+            break;
+        case "clave2":
+            validaClave();
+            break;
+    }
+}
+
+function validaClave() {
+    if (document.getElementById(`clave`).value == document.getElementById(`clave2`).value) {
+        document.getElementById(`grupo__clave2`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__clave2`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__clave2 i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__clave2 i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__clave2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos['clave2'] = true;
+    } else {
+        document.getElementById(`grupo__clave2`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__clave2`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__clave2 i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__clave2 i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__clave2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos['clave2'] = false;
     }
 }
 
@@ -54,7 +82,8 @@ formulario.addEventListener('submit', (e) => { //QUE AL PRESIONAR EL BOTON ENVIA
     e.preventDefault();
 
     const terminos = document.getElementById('terminos'); // VALIDAR EN EL CHECK DE LOS TERMINOS QUE VALIDE SI EL RESTO ESTA BIEN, PARA ASI ENVIAR EL MENSAJE DE EXITO
-    if (campos.nombre && campos.correo && campos.telefono && terminos.checked) {
+    // da error en la consola el checked nosepq if (campos.nombre && campos.correo && campos.telefono && terminos.checked) {
+    if (campos.nombre && campos.correo && campos.telefono && campos.clave && campos.clave2) { 
         formulario.reset();
 
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
