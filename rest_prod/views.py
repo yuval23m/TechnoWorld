8ec110677709d16960046edc3ed5cc8c52ba399f
lete <103456882+LETEX007@users.noreply.github.com>
2022-05-25 15:57:23 -0400

    CLASE MIERCOLES 25/05

--- rest_prod/views.py
+++ rest_prod/views.py
@@ -22,3 +22,25 @@ def lista_prod(request):
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def detalle_prod(request,ID):
+    try:
+        producto = Producto.objects.get(idpro=ID)
+    except Producto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = ProdSerializer(producto)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        dataP = JSONParser().parse(request)
+        serializer = ProdSerializer(producto, data = dataP)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        producto.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
