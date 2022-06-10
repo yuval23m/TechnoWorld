d934f662b1b7d924eafb0262b45e502121b42470
lete <103456882+LETEX007@users.noreply.github.com>
2022-06-10 02:20:03 -0400

    Help text None

--- knoxapp/serializers.py
+++ knoxapp/serializers.py
@@ -23,8 +23,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username','email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
+        extra_kwargs = {'password': {'write_only': True},
+                        'username': {'help_text': None}}
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'],validated_data['email'], validated_data['password'])
 
