2e2fd5743bdfae358910d64843a0354872bc85db
lete <103456882+LETEX007@users.noreply.github.com>
2022-06-24 00:07:59 -0400

    ElCarrito BETA REST FRAMEWORK

--- productos/admin.py
+++ productos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cliente, TipoProducto, Producto
+from .models import CartItem, Cliente, TipoProducto, Producto
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(TipoProducto)
 admin.site.register(Producto)
+admin.site.register(CartItem)
--- rest_prod/serializers.py
+++ rest_prod/serializers.py
@@ -1,7 +1,50 @@
 from rest_framework import serializers
-from productos.models import Producto
+from productos.models import Producto, CartItem
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.fields import CurrentUserDefault
+
 
 class ProdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
-        fields = ['nombre','precio','img','marca','tipo']
+        fields = ['idpro','nombre','precio','img','marca','tipo','cantidad','is_available']
+class CartItemSerializer(serializers.ModelSerializer):
+    """
+    serializer for cartitem that serialize all fields in 'CartItem' class
+    model and add 'product' as relation
+    """
+
+    producto = ProdSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = ('id', 'cantidad', 'producto')
+        
+class CartItemAddSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = CartItem
+        fields = ('id','cantidad')
+        extra_kwargs = {
+            'cantidad': {'required': True},
+            'id': {'required': True},
+        }
+        
+    def create(self, validated_data):
+
+        user = User.objects.get(id=self.context['request'].user.id)
+        producto = get_object_or_404(Producto, idpro=validated_data['id'])
+        if producto.cantidad == 0 or producto.is_available is False:
+            raise serializers.ValidationsError(
+                {'not available': 'the product is not available.'})
+
+        cart_item = CartItem.objects.create(
+            producto=producto,
+            user=user,
+            cantidad=validated_data['cantidad']
+            )
+        cart_item.save()
+        producto.cantidad = producto.cantidad - cart_item.cantidad
+        producto.save()
+        return cart_item
